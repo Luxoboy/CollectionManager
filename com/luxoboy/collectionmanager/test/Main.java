cf5395d829d5b33ea08d394a0c9efa552ce5dc08
Anthony Correia <anthony.correia71@gmail.com>
2015-02-09 10:28:48 +0100

    Implemented search/tv API request. Tested and working.

---
+++ com/luxoboy/collectionmanager/test/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.luxoboy.collectionmanager.test;
+
+import com.luxoboy.collectionmanager.api.SearchTV;
+
+/**
+ *
+ * @author Anthony Correia
+ */
+public class Main
+{
+    public static void main(String args[])
+    {
+        SearchTV s = new SearchTV("community");
+        s.proceed();
+    }
+    
+    
+}
