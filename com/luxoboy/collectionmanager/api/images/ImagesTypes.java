65e37bd53279ad8ab52586cce7240bf153cf51b2
Anthony Correia <anthony.correia71@gmail.com>
2015-02-10 22:21:24 +0100

    Added ImageTypes enum.

---
+++ com/luxoboy/collectionmanager/api/images/ImagesTypes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.luxoboy.collectionmanager.api.images;
+
+/**
+ *
+ * @author Anthony Correia
+ */
+public enum ImagesTypes
+{
+    Backdrop,
+    Logo,
+    Poster,
+    Profile,
+    Still;
+}
