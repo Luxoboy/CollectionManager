4e3bf4dddc151dd40c4a6df12bbf528691ce6a70
Anthony Correia <anthony.correia71@gmail.com>
2015-02-10 22:07:04 +0100

    Added Configuration class. This class will implement the configuration data fetching.

---
+++ com/luxoboy/collectionmanager/api/Configuration.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.luxoboy.collectionmanager.api;
+
+/**
+ *
+ * @author Anthony Correia
+ */
+public class Configuration extends ApiRequest
+{
+
+    public Configuration(String RESSOURCE_URL)
+    {
+        super(RESSOURCE_URL);
+    }
+    
+    
+    
+}
