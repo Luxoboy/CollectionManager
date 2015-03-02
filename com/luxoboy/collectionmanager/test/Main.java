6e2e32ceba4abf12c77d5c41c062510464bad39c
Anthony Correia <anthony.correia71@gmail.com>
2015-03-02 12:14:56 +0100

    Deleted pom.xml (YEEEES)

--- com/luxoboy/collectionmanager/test/Main.java
+++ com/luxoboy/collectionmanager/test/Main.java
@@ -5,7 +5,16 @@
  */
 package com.luxoboy.collectionmanager.test;
 
+import com.luxoboy.collectionmanager.api.Configuration;
 import com.luxoboy.collectionmanager.api.SearchTV;
+import com.luxoboy.collectionmanager.api.TVDetails;
+import com.luxoboy.collectionmanager.api.images.Images;
+import com.luxoboy.collectionmanager.api.images.ImageSizes;
+import com.luxoboy.collectionmanager.api.images.ImageSizes.SizeList;
+import com.luxoboy.collectionmanager.api.images.ImageTypes;
+import com.luxoboy.collectionmanager.api.model.TVShow;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,9 +24,20 @@ public class Main
 {
     public static void main(String args[])
     {
-        SearchTV s = new SearchTV("community");
-        s.proceed();
+        try
+        {
+            Configuration.proceed();
+            SearchTV s = new SearchTV("breaking bad");
+            TVDetails details = new TVDetails();
+            for(TVShow tvs : s.proceed())
+            {
+                details.proceed(tvs.getId());
+            }
+        } catch (Exception ex)
+        {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        //Image.downloadImage(ImageTypes.Backdrop, ImageSizes.Original, "4mW6xxN8pHtfECWGNHQQmoP34J.jpg", true);
     }
-    
-    
 }
